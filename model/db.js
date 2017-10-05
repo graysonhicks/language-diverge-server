60153a2479e3d20638fca3a3756bec1107d78765
Grayson Hicks <graysonhicks@gmail.com>
2017-10-05 09:44:06 -0400

    new connection

--- model/db.js
+++ model/db.js
@@ -1,22 +1,29 @@
 var mongoose = require("mongoose");
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
-var computerSchema = new mongoose.Schema(
-	{
+mongoose.connect(databaseUri, {
+	useMongoClient: true
+});
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+	var computerSchema = new mongoose.Schema(
+		{
+			Year: String,
+			Country: String,
+			Languages: String
+		},
+		{
+			collection: "stackoverflow"
+		}
+	);
+
+	var humanSchema = new mongoose.Schema({
 		Year: String,
 		Country: String,
 		Languages: String
-	},
-	{
-		collection: "stackoverflow"
-	}
-);
-
-var humanSchema = new mongoose.Schema({
-	Year: String,
-	Country: String,
-	Languages: String
+	});
+	mongoose.model("Computer", computerSchema);
+	mongoose.model("Human", humanSchema);
 });
-mongoose.model("Computer", computerSchema);
-mongoose.model("Human", humanSchema);
-mongoose.connect(databaseUri);
--- routes/computers.js
+++ routes/computers.js
@@ -3,6 +3,8 @@ var computerData = require("../model/computers");
 exports.list = function(req, res) {
 	var year = "2016";
 	computerData.computerList(year, function(err, computerList) {
+		console.log(err);
+		console.log(computerList);
 		res.send({
 			title: "Test web page on node.js using Express and Mongoose",
 			pagetitle: "Hello there",
