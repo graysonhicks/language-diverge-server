54b80a80861b71c6858b0c6ccd50a5a590af1435
Grayson Hicks <graysonhicks@gmail.com>
2017-10-05 09:22:17 -0400

    cleaning

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ var express = require("express");
 var routes = require("./routes");
 var db = require("./model/db");
 var request = require("request");
-var fs = require("fs");
 var app = express();
-var cors = require("cors");
-
-var appRoot = process.cwd();
 
 // Routes
 
 var humans = require("./routes/humans");
 var computers = require("./routes/computers");
-
-app.use(cors()); //allows overriding cross origin policy (use npm install if needed)
 
 app.get("/", routes.index);
 
