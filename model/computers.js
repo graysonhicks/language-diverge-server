a4acd2c529345d4373c0887ef2d2140af96ce767
Grayson Hicks <graysonhicks@gmail.com>
2017-10-05 13:37:48 -0400

    changed find

--- model/computers.js
+++ model/computers.js
@@ -1,12 +1,15 @@
 exports.computerList = function computerList(year, callback) {
-	db.collection("stackoverflow").find({
-		Year: year
-	}, function(err, computers) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(computers);
-			callback("", computers);
-		}
-	}); // end Computer.find
+	db
+		.collection("stackoverflow")
+		.find({
+			Year: year
+		})
+		.toArray(function(err, computers) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(computers);
+				callback("", computers);
+			}
+		}); // end Computer.find
 }; // end exports.computerList
