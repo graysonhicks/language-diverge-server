f9397e612ba239220be07640d6fd51916bbc5fdb
Grayson Hicks <graysonhicks@gmail.com>
2017-10-05 15:55:27 -0400

    unique labels

--- routes/computers.js
+++ routes/computers.js
@@ -2,9 +2,17 @@ var computerData = require("../model/computers");
 
 exports.list = function(req, res) {
 	computerData.computerList(req.params, function(err, computerList) {
-		var labels = computerList.map(function(item) {
-			return item.Year;
-		});
+		var labels = function unique(computerList) {
+			var hash = {},
+				result = [];
+			for (var i = 0, l = computerList.length; i < l; ++i) {
+				if (!hash.hasOwnProperty(computerList[i])) {
+					hash[computerList[i]] = true;
+					result.push(computerList[i]);
+				}
+			}
+			return result;
+		};
 		res.send({
 			labels: labels,
 			computers: computerList
