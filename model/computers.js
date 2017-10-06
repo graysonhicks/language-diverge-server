3075004129ab6c87f8f8a8bc2393b34109aff1a1
Grayson Hicks <graysonhicks@gmail.com>
2017-10-06 10:15:14 -0400

    gonna rework excel for refined data

--- model/computers.js
+++ model/computers.js
@@ -1,45 +1,48 @@
 function unique(arr) {
-  var o = {},
-    i,
-    l = arr.length,
-    r = [];
-  for (i = 0; i < l; i += 1)
-    o[arr[i]] = arr[i];
-  for (i in o)
-    r.push(o[i]);
-  return r;
+	var o = {},
+		i,
+		l = arr.length,
+		r = [];
+	for (i = 0; i < l; i += 1) o[arr[i]] = arr[i];
+	for (i in o) r.push(o[i]);
+	return r;
 }
 
 exports.computerList = function computerList(params, callback) {
-  db.collection("stackoverflow").find({ "Year": 2015}).toArray(function(err, computers) {
-    if (err) {
-      console.log(err);
-    } else {
-      var newData = {};
-      computers.map(function(item) {
-        var itemLanguagesArray = item.Languages.split(";");
-        var itemCountry = item.Country;
-        var itemYear = item.Year;
+	var years = db.collection("stackoverflow").distinct("Year", function(err, years) {
+		db
+			.collection("stackoverflow")
+			.find()
+			.toArray(function(err, computers) {
+				if (err) {
+					console.log(err);
+				} else {
+					var newData = {};
+					computers.map(function(item) {
+						var itemLanguagesArray = item.Languages.split(";");
+						var itemCountry = item.Country;
+						var itemYear = item.Year;
 
-        for (var i = 0; i < itemLanguagesArray.length; i++) {
-          var languageObject = {
-            "language": itemLanguagesArray[i].trim().toLowerCase(),
-            "country": itemCountry,
-            "year": itemYear
-          }
-          if (!newData[itemLanguagesArray[i].trim().toLowerCase()]) {
-            newData[itemLanguagesArray[i].trim().toLowerCase()] = [];
-          }
-          newData[itemLanguagesArray[i].trim().toLowerCase()].push(languageObject);
-        }
-      });
-			// getting count of each language
-			var result = {};
-			Object.keys(newData).forEach(function (key) {
-				result[key] = newData[key].length;
-			})
+						for (var i = 0; i < itemLanguagesArray.length; i++) {
+							var languageObject = {
+								language: itemLanguagesArray[i].trim().toLowerCase(),
+								country: itemCountry,
+								year: itemYear
+							};
+							if (!newData[itemYear]) {
+								newData[itemYear] = [];
+							}
+							newData[itemYear].push(languageObject);
+						}
+					});
+					// getting count of each language
+					var result = {};
+					Object.keys(newData).forEach(function(key) {
+						result[key] = newData[key].length;
+					});
 
-      callback("", newData);
-    }
-  }); // end Computer.find
+					callback("", newData);
+				}
+			}); // end Computer.find
+	});
 }; // end exports.computerList
