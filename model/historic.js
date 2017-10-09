af2f2ea8bd4952c1d92b2c81db44db34a19d8f3f
Grayson Hicks <graysonhicks@gmail.com>
2017-10-09 10:02:35 -0400

    sorting test

--- model/historic.js
+++ model/historic.js
@@ -2,6 +2,7 @@ exports.historicList = function historicList(params, callback) {
 	db
 		.collection("historic")
 		.find()
+		.sort({ Year: 1 })
 		.toArray(function(err, languages) {
 			if (err) {
 				console.log(err);
