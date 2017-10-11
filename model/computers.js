f24e55b7249b178f96c461422ab04c0749b221a2
Grayson Hicks <graysonhicks@gmail.com>
2017-10-11 10:38:45 -0400

    removed parens

--- model/computers.js
+++ model/computers.js
@@ -1,10 +1,12 @@
 exports.computerList = function computerList(params, callback) {
-	db.collection("stackoverflow").find().toArray(function(err, computers) {
-	    if (err) {
-	        console.log(err);
-	    } else {
-	        callback("", computers);
-	    }
-	}); // end Computer.find
-	});
+	db
+		.collection("stackoverflow")
+		.find()
+		.toArray(function(err, computers) {
+			if (err) {
+				console.log(err);
+			} else {
+				callback("", computers);
+			}
+		}); // end Computer.find
 }; // end exports.computerList
