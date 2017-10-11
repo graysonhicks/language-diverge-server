5d4808b9c0d69c22ec7106ef32e7f44ee3330158
Grayson Hicks <graysonhicks@gmail.com>
2017-10-11 09:42:15 -0400

    sending as obj

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ exports.index = function(req, res, mjs) {
 	mjs.get("home", function(err, v) {
 		if (v) {
 			console.log(v);
-			res.send(v.toString());
+			res.send({ data: v.toString() });
 		} else {
 			var newV = {
 				title: "Test web page on node.js using Express",
@@ -12,7 +12,7 @@ exports.index = function(req, res, mjs) {
 			};
 			newV = newV.toString();
 			mjs.set("home", newV, function(err, val) {
-				res.send(val.toString());
+				res.send({ data: val.toString() });
 			});
 		}
 	});
