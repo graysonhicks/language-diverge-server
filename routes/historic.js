92e36e0756149f25f97f92dedc3d0b7e4d985766
Grayson Hicks <graysonhicks@gmail.com>
2017-10-11 09:22:48 -0400

    adding v log

--- routes/historic.js
+++ routes/historic.js
@@ -4,6 +4,7 @@ exports.list = function(req, res, mjs) {
 	historicData.historicList(req.params, function(err, historicList) {
 		mjs.get("historic", function(err, v) {
 			if (v) {
+				console.log(v);
 				res.send(v.toString());
 			} else {
 				res.send({
