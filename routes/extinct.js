6c86326b7fa963e7179eee1b1344a74f43cc5dce
Grayson Hicks <graysonhicks@gmail.com>
2017-10-19 09:51:38 -0400

    added extinct to memcache

--- routes/extinct.js
+++ routes/extinct.js
@@ -1,9 +1,27 @@
 var extinctData = require("../model/extinct");
 
-exports.list = function(req, res) {
-	extinctData.exinctList(req.params, function(err, exinctList) {
-		res.send({
-			data: exinctList
+exports.list = function(req, res, mjs) {
+	endangeredData.exinctList(req.params, function(err, exinctList) {
+		mjs.get("extinct", function(err, v) {
+			if (v) {
+				var json = JSON.parse(v.toString());
+				res.send({ data: json });
+			} else {
+				var newV = exinctList;
+				newV = JSON.stringify(newV);
+				mjs.set("endangered", newV, function(err, val) {
+					if (err) {
+						console.log("Error setting key: " + err);
+						res.render("error", {
+							message: err.message,
+							error: err
+						});
+					} else {
+						var json = JSON.parse(val.toString());
+						res.send({ data: json });
+					}
+				});
+			}
 		});
 	});
 };
