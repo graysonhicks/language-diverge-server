e06e7ad1a69f124dff78b29f06de65bd8737c0b3
Grayson Hicks <graysonhicks@gmail.com>
2017-10-20 09:34:57 -0400

    extinct mapping unique

--- model/extinct.js
+++ model/extinct.js
@@ -6,7 +6,28 @@ exports.extinctList = function extinctList(params, callback) {
 			if (err) {
 				console.log(err);
 			} else {
-				callback("", languages);
+				db.collection("extinct").distinct("year", function(err, uniqueYears) {
+					if (err) {
+						console.log(err);
+					} else {
+						languages = languages.map(item => {
+							var years = [];
+							for (var i = 0; i < uniqueYears.length; i++) {
+								if (item.year == uniqueYears[i]) {
+									years.push(1);
+								} else {
+									years.push(0);
+								}
+							}
+							return {
+								label: item.language,
+								backgroundColor: colors[Math.floor(Math.random() * colors.length)],
+								data: years
+							};
+						});
+						callback("", { languages: languages, uniqueYears: uniqueYears });
+					}
+				});
 			}
 		}); // end extinct.find
 }; // end exports.extinctList
