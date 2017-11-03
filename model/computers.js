e69edc8daf469e5c9e4128cd0b7892fbc7da9029
Grayson Hicks <graysonhicks@gmail.com>
2017-11-03 09:32:23 -0400

    all years

--- model/computers.js
+++ model/computers.js
@@ -1,9 +1,16 @@
 var _ = require("underscore");
 
+var collections = ["so2011", "so2012", "so2013", "so2014", "so2015", "so2016"];
+
 var computers = {
-	"2011": [],
-	"2012": []
+	so2011: [],
+	so2012: [],
+	so2013: [],
+	so2014: [],
+	so2015: [],
+	so2016: []
 };
+
 exports.computerList = function computerList(params, callback) {
 	db
 		.collection("so2011")
@@ -35,8 +42,76 @@ exports.computerList = function computerList(params, callback) {
 									}
 								})
 							);
+							db
+								.collection("so2013")
+								.find()
+								.toArray(function(err, so2013) {
+									if (err) {
+										console.log(err);
+									} else {
+										computers["2013"] = _.countBy(
+											so2013.map(item => {
+												var languages = item.languages.split(";");
+												for (var i = 0; i < languages.length; i++) {
+													return languages[i];
+												}
+											})
+										);
+										db
+											.collection("so2014")
+											.find()
+											.toArray(function(err, so2014) {
+												if (err) {
+													console.log(err);
+												} else {
+													computers["2014"] = _.countBy(
+														so2014.map(item => {
+															var languages = item.languages.split(";");
+															for (var i = 0; i < languages.length; i++) {
+																return languages[i];
+															}
+														})
+													);
+													db
+														.collection("so2015")
+														.find()
+														.toArray(function(err, so2015) {
+															if (err) {
+																console.log(err);
+															} else {
+																computers["2015"] = _.countBy(
+																	so2015.map(item => {
+																		var languages = item.languages.split(";");
+																		for (var i = 0; i < languages.length; i++) {
+																			return languages[i];
+																		}
+																	})
+																);
+																db
+																	.collection("so2016")
+																	.find()
+																	.toArray(function(err, so2016) {
+																		if (err) {
+																			console.log(err);
+																		} else {
+																			computers["2016"] = _.countBy(
+																				so2016.map(item => {
+																					var languages = item.languages.split(";");
+																					for (var i = 0; i < languages.length; i++) {
+																						return languages[i];
+																					}
+																				})
+																			);
 
-							callback("", computers);
+																			callback("", computers);
+																		}
+																	}); // end Computer.find
+															}
+														}); // end Computer.find
+												}
+											}); // end Computer.find
+									}
+								}); // end Computer.find
 						}
 					}); // end Computer.find
 			}
