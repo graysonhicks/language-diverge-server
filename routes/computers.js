0a7301849bce6867ea6ab76703fc46dda7eb11bc
Grayson Hicks <graysonhicks@gmail.com>
2017-11-10 08:14:51 -0500

    full so data back on, will need to reimport mongo

--- routes/computers.js
+++ routes/computers.js
@@ -2,28 +2,27 @@ var computerData = require("../model/computers");
 
 exports.list = function(req, res, mjs) {
 	computerData.computerList(req.params, function(err, computersList) {
-		res.send({ data: computersList });
-		// mjs.get("computer", function(err, v) {
-		// 	if (v) {
-		// 		var json = JSON.parse(v.toString());
-		// 		console.log(json);
-		// 		res.send({ data: json });
-		// 	} else {
-		// 		var newV = computersList;
-		// 		newV = JSON.stringify(newV);
-		// 		mjs.set("computer", newV, function(err, val) {
-		// 			if (err) {
-		// 				console.log("Error setting key: " + err);
-		// 				res.render("error", {
-		// 					message: err.message,
-		// 					error: err
-		// 				});
-		// 			} else {
-		// 				var json = JSON.parse(val.toString());
-		// 				res.send({ data: json });
-		// 			}
-		// 		});
-		// 	}
-		// });
+		mjs.get("computer", function(err, v) {
+			if (v) {
+				var json = JSON.parse(v.toString());
+				console.log(json);
+				res.send({ data: json });
+			} else {
+				var newV = computersList;
+				newV = JSON.stringify(newV);
+				mjs.set("computer", newV, function(err, val) {
+					if (err) {
+						console.log("Error setting key: " + err);
+						res.render("error", {
+							message: err.message,
+							error: err
+						});
+					} else {
+						var json = JSON.parse(val.toString());
+						res.send({ data: json });
+					}
+				});
+			}
+		});
 	});
 };
